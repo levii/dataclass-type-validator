b25f1b1a1eea94aa39c5941e7cf0d8f4a2c27a9f
Michael Lamb <mr.lamby@gmail.com>
2021-10-10 20:41:19 +1100

    Address comments raised by the sider review

--- dataclass_type_validator/__init__.py
+++ dataclass_type_validator/__init__.py
@@ -6,7 +6,7 @@ from typing import Any
 from typing import Optional
 from typing import Dict
 
-GlobalNS_Type = Dict[str, Any]
+GlobalNS_T = Dict[str, Any]
 
 class TypeValidationError(Exception):
     """Exception raised on type validation errors.
@@ -43,43 +43,46 @@ def _validate_type(expected_type: type, value: Any) -> Optional[str]:
         return f'must be an instance of {expected_type}, but received {type(value)}'
 
 
-def _validate_iterable_items(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_Type) -> Optional[str]:
+def _validate_iterable_items(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_T) -> Optional[str]:
     expected_item_type = expected_type.__args__[0]
-    errors = [_validate_types(expected_type=expected_item_type, value=v, strict=strict, globalns=globalns) for v in value]
+    errors = [_validate_types(expected_type=expected_item_type, value=v, strict=strict, globalns=globalns) 
+              for v in value]
     errors = [x for x in errors if x]
     if len(errors) > 0:
         return f'must be an instance of {expected_type}, but there are some errors: {errors}'
 
 
-def _validate_typing_list(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_Type) -> Optional[str]:
+def _validate_typing_list(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_T) -> Optional[str]:
     if not isinstance(value, list):
         return f'must be an instance of list, but received {type(value)}'
     return _validate_iterable_items(expected_type, value, strict, globalns)
 
 
-def _validate_typing_tuple(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_Type) -> Optional[str]:
+def _validate_typing_tuple(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_T) -> Optional[str]:
     if not isinstance(value, tuple):
         return f'must be an instance of tuple, but received {type(value)}'
     return _validate_iterable_items(expected_type, value, strict, globalns)
 
 
-def _validate_typing_frozenset(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_Type) -> Optional[str]:
+def _validate_typing_frozenset(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_T) -> Optional[str]:
     if not isinstance(value, frozenset):
         return f'must be an instance of frozenset, but received {type(value)}'
     return _validate_iterable_items(expected_type, value, strict, globalns)
 
 
-def _validate_typing_dict(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_Type) -> Optional[str]:
+def _validate_typing_dict(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_T) -> Optional[str]:
     if not isinstance(value, dict):
         return f'must be an instance of dict, but received {type(value)}'
 
     expected_key_type = expected_type.__args__[0]
     expected_value_type = expected_type.__args__[1]
 
-    key_errors = [_validate_types(expected_type=expected_key_type, value=k, strict=strict, globalns=globalns) for k in value.keys()]
+    key_errors = [_validate_types(expected_type=expected_key_type, value=k, strict=strict, globalns=globalns) 
+                  for k in value.keys()]
     key_errors = [k for k in key_errors if k]
 
-    val_errors = [_validate_types(expected_type=expected_value_type, value=v, strict=strict, globalns=globalns) for v in value.values()]
+    val_errors = [_validate_types(expected_type=expected_value_type, value=v, strict=strict, globalns=globalns) 
+                  for v in value.values()]
     val_errors = [v for v in val_errors if v]
 
     if len(key_errors) > 0 and len(val_errors) > 0:
@@ -91,7 +94,7 @@ def _validate_typing_dict(expected_type: type, value: Any, strict: bool, globaln
         return f'must be an instance of {expected_type}, but there are some errors in values: {val_errors}'
 
 
-def _validate_typing_callable(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_Type) -> Optional[str]:
+def _validate_typing_callable(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_T) -> Optional[str]:
     _ = strict
     if not isinstance(value, type(lambda a: a)):
         return f'must be an instance of {expected_type._name}, but received {type(value)}'
@@ -112,7 +115,7 @@ _validate_typing_mappings = {
 }
 
 
-def _validate_sequential_types(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_Type) -> Optional[str]:
+def _validate_sequential_types(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_T) -> Optional[str]:
     validate_func = _validate_typing_mappings.get(expected_type._name)
     if validate_func is not None:
         return validate_func(expected_type, value, strict, globalns)
@@ -132,12 +135,13 @@ def _validate_sequential_types(expected_type: type, value: Any, strict: bool, gl
         raise RuntimeError(f'Unknown type of {expected_type} (_name = {expected_type._name})')
 
 
-def _validate_types(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_Type) -> Optional[str]:
+def _validate_types(expected_type: type, value: Any, strict: bool, globalns: GlobalNS_T) -> Optional[str]:
     if isinstance(expected_type, type):
         return _validate_type(expected_type=expected_type, value=value)
 
     if isinstance(expected_type, typing._GenericAlias):
-        return _validate_sequential_types(expected_type=expected_type, value=value, strict=strict, globalns=globalns)
+        return _validate_sequential_types(expected_type=expected_type, value=value,
+                                          strict=strict, globalns=globalns)
 
     if isinstance(expected_type, typing.ForwardRef):
         referenced_type = expected_type._evaluate(globalns, None, set())
